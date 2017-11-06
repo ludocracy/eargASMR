c22a444cc3472c2d34d1a06eac03c497cee00ad7
Andrew Chavez <chavandrew@gmail.com>
2017-11-06 16:37:59 -0600

    code for sound being able to be played fom URL

--- components/SoundButton.js
+++ components/SoundButton.js
@@ -4,13 +4,39 @@ import {
   View,
   Button
 } from 'react-native';
+import Sound from 'react-native-sound'
 
 export default class SoundButton extends Component<{}> {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      isPlaying: false
+    }
+
+    this.sound = new Sound('https://www.memoclic.com/medias/sons-wav/2/705.wav');
+
+    this._onPress = this._onPress.bind(this);
+  }
+
+  _onPress() {
+
+    if(this.state.isPlaying) {
+      this.sound.pause()
+    } else {
+      this.sound.play();
+    }
+
+    this.setState ({
+      isPlaying: !this.state.isPlaying
+    });
+  }
+
   render() {
     return (
       <View style={styles.soundButton}>
         <Button
-          onPress ={() => {}}
+          onPress ={this._onPress}
           title='Sound Button'
           color="#841584"
         />
