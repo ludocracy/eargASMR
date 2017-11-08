7dd780fb0b82695914ecb408a1baab1d149bddb7
Dazaev Vives <dazaev.vives.cervantes@gmail.com>
2017-11-08 15:32:48 -0600

    got rid of old icon files

--- assets/images.js
+++ assets/images.js
@@ -1,15 +1,15 @@
 
 
 const images = {
-	bell: require("./img/bells.png"),
-	bird: require("./img/birds.png"),
-	fire: require("./img/bonfires.png"),
-	cicada: require("./img/cicadas.png"),
-	cricket: require("./img/crickets.png"),
-	leaves: require("./img/leavess.png"),
-	night: require("./img/nights.png"),
-	waves: require("./img/wavess.png"),
-	rain: require("./img/wind-rains.png"),
+	bell: require("./img/bell.png"),
+	bird: require("./img/bird.png"),
+	fire: require("./img/bonfire.png"),
+	cicada: require("./img/cicada.png"),
+	cricket: require("./img/cricket.png"),
+	leaves: require("./img/leaves.png"),
+	night: require("./img/night.png"),
+	waves: require("./img/waves.png"),
+	rain: require("./img/wind-rain.png"),
 };
 
 
