d68a8dae7dcb48b99a08fcde0663c89a174bc17e
Andrew Chavez <chavandrew@gmail.com>
2017-11-08 11:35:05 -0600

    able to switch between pages

--- components/ControlPanel.js
+++ components/ControlPanel.js
@@ -11,50 +11,49 @@ import {
 } from 'react-native';
 import SoundControl from './SoundControl';
 import Timer from './Timer';
-import SlidingUpPanel from 'rn-sliding-up-panel';
 
 
 export default class ControlPanel extends Component<{}> {
-  constructor(props){
-    super(props);
-    this.time = 5;
-    this.state = {
-      seconds: 0,
-    };
-    // this.reset = this.reset.bind(this);
-    // this.start = this.start.bind(this);
-    // this.pause = this.pause.bind(this);
-  }
-
-  startTimer(e){
-    e.preventDefault();
-    this.interval = setInterval(() => {
-      this.setState({
-        seconds: this.state.seconds - 1,
-      });
-    }, 1000);
-  }
-
-  stopTimer(e){
-    e.preventDefault();
-    if(this.interval) {
-      clearInterval(this.interval)
-      this.interval = null;
-    } else {
-      this.start();
-    }
-  }
-
-  resetTimer(e){
-    e.preventDefault();
-    if(this.interval){
-      clearInterval(this.interval)
-      this.interval = null;
-    }
-    this.setState({
-      seconds: 0,
-    });
-  }
+  // constructor(props){
+  //   super(props);
+  //   this.time = 5;
+  //   this.state = {
+  //     seconds: 0,
+  //   };
+  //   // this.reset = this.reset.bind(this);
+  //   // this.start = this.start.bind(this);
+  //   // this.pause = this.pause.bind(this);
+  // }
+  //
+  // startTimer(e){
+  //   e.preventDefault();
+  //   this.interval = setInterval(() => {
+  //     this.setState({
+  //       seconds: this.state.seconds - 1,
+  //     });
+  //   }, 1000);
+  // }
+  //
+  // stopTimer(e){
+  //   e.preventDefault();
+  //   if(this.interval) {
+  //     clearInterval(this.interval)
+  //     this.interval = null;
+  //   } else {
+  //     this.start();
+  //   }
+  // }
+  //
+  // resetTimer(e){
+  //   e.preventDefault();
+  //   if(this.interval){
+  //     clearInterval(this.interval)
+  //     this.interval = null;
+  //   }
+  //   this.setState({
+  //     seconds: 0,
+  //   });
+  // }
 
   render() {
     return (
